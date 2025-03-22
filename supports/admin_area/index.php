6fe0ea4e2fd63c986b930c252b53b7ef1700547b
tashfianizum <tashfia.nizum@northsouth.edu>
2025-03-22 23:42:52 +0600

    Add files via upload

--- supports/admin_area/index.php
+++ supports/admin_area/index.php
@@ -11,9 +11,8 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
      rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="register.php">
-    <link rel="stylesheet" href="login.php">
+     <link rel="stylesheet" href="../style.css">
+    
     <style>
         .admin_img {
       width: 100px;
